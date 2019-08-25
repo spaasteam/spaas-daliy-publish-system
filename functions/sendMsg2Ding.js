23ed8c577d2363b0bbdc59e5f9cd331824b2df5e
cjfff <865553742@qq.com>
2019-08-25 13:10:34 +0800

    fix: fix bug

--- functions/sendMsg2Ding.js
+++ functions/sendMsg2Ding.js
@@ -1,9 +1,15 @@
 const axios = require("axios");
-
+const base = "https://oapi.dingtalk.com/robot/send";
 exports.handler = async function(event, context, cb) {
+
   await axios.post(
     `${base}?access_toekn=7cd2c0105cc43f47486078ef15b73b8e0eb59a9763524627da74a5d37cec5df9`,
-    event.body
+    event.body,
+    {
+      headers: {
+        contentType: 'appliction/json'
+      }
+    }
   );
 
   cb(null, {
--- src/services/v1/dingApi.js
+++ src/services/v1/dingApi.js
@@ -1,5 +1,6 @@
 import axios from "@/services";
 
+// const apibase = `http://127.0.0.1:9000/sendMsg2Ding`;
+const apibase = "/.netlify/functions/sendMsg2Ding";
 // 发送消息到群聊
-export const postMessage2Group = params =>
-         axios.$post(`/.netlify/functions/sendMsg2Ding`, params);
+export const postMessage2Group = params => axios.$post(apibase, { params });
