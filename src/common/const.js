492d9d34a276c8f120a03c4b9733f055da0973b7
cjfff <cjfff1996@gmali.com>
2020-03-16 08:51:56 +0800

    chore: app name change

--- src/common/const.js
+++ src/common/const.js
@@ -3,7 +3,7 @@ export const DING_TOKEN = process.env.DING_TOKEN;
 export const isDev = process.env.NODE_ENV === "development";
 
 export const SYSTEM_NAME =
-  process.env.VUE_APP_SYSTEM_NAME || "SPaas 每日一题发布系统";
+  process.env.VUE_APP_APP_NAME || "SPaas 每日一题发布系统";
 
 // 仓库 owner
 export const OWNER = process.env.VUE_APP_GITHUB_OWNER;
