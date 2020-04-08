f1760dd473c69a68da271b778ebb1056901cb450
cjfff <cjfff1996@gmali.com>
2020-04-08 22:44:55 +0800

    feat: add 404页面

--- src/views/404/index.vue
+++ src/views/404/index.vue
@@ -1,12 +1,36 @@
 <template>
-  <div class="404-page">
-    <h2>the page not found</h2>
-    <el-button @click="$router.push('/')">返回首页</el-button>
+  <div class="error-wrapper">
+    <div>
+      <img
+        src="//deepexi.oss-cn-shenzhen.aliyuncs.com/deepexi-services/%E5%85%AC%E5%85%B1/404.png"
+        width="440px"
+      />
+      <br />
+      <div class="text">抱歉，你访问的页面不存在。</div>
+    </div>
+    <el-button @click="goHome" type="primary" round>返回首页</el-button>
   </div>
 </template>
 
 <script>
-export default {};
+export default {
+  methods: {
+    goHome() {
+      this.$router.push("/");
+    }
+  }
+};
 </script>
 
-<style></style>
+<style lang="less" scoped>
+.error-wrapper {
+  padding-top: 80px;
+  text-align: center;
+  .text {
+    font-size: 14px;
+    color: rgba(45, 48, 59, 0.4);
+    line-height: 20px;
+    margin-bottom: 24px;
+  }
+}
+</style>
